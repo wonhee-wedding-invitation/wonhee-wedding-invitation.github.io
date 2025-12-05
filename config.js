0cda470523d04f026325bcfd88368bba841183e9
coding frog <woojw6012@gmail.com>
2025-12-05 12:38:04 +0900

    init

---
+++ config.js
@@ -0,0 +1,21 @@
+export const WEDDING_INVITATION_URL = "https://codingkermit.github.io/wedding-invitation/";
+export const KAKAOTALK_API_TOKEN = "1f3eb139839646ade48bb4cce0503327";
+export const KAKAOTALK_SHARE_IMAGE =
+  "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
+
+export const WEDDING_DATE = "2026년 02월 07일, 토요일 오후 02시 00분";
+export const WEDDING_LOCATION = "강동KDW웨딩, 블랙스톤홀";
+
+export const GROOM_NAME = "우지원";
+export const GROOM_ACCOUNT_NUMBER = "신한은행 110-441-025430";
+export const GROOM_FATHER_NAME = "우경하";
+export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_MOTHER_NAME = "강선희";
+export const GROOM_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+
+export const BRIDE_NAME = "김태희";
+export const BRIDE_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const BRIDE_FATHER_NAME = "김성일";
+export const BRIDE_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const BRIDE_MOTHER_NAME = "박서영";
+export const BRIDE_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pathPrefix: "/wedding-invitation",
+  siteMetadata: {
+    title: `weddingInvitation`,
+    siteUrl: `https://codingkermit.github.io/wedding-invitation/`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
