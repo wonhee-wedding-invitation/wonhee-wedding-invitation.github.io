ca6a164e6fe364643221b787f6e730648c6c4b2b
wonhee-wedding-invitation <woojw0829@gmail.com>
2025-12-06 00:23:47 +0900

    Fix: map location fix

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -47,11 +47,11 @@ const Location = () => {
   const executeScript = () => {
     const scriptTag = document.createElement("script");
     const inlineScript = document.createTextNode(`new daum.roughmap.Lander({
-    "timestamp" : "1652464367301",
-    "key" : "2a8fe",
-    "mapWidth" : "640",
-    "mapHeight" : "360"
-  }).render();`);
+		"timestamp" : "1764947291562",
+		"key" : "drzb3smi3v8",
+		"mapWidth" : "640",
+		"mapHeight" : "360"
+	}).render();`);
     scriptTag.appendChild(inlineScript);
     document.body.appendChild(scriptTag);
   };
@@ -103,23 +103,15 @@ const Location = () => {
         className="root_daum_roughmap root_daum_roughmap_landing"
       ></Map>
       <Content>
-        대구 수성구 두산동 888-2번지
+        서울 강동구 천호대로 1102
         <br />
-        호텔수성 수성스퀘어 3층 피오니홀
-        <br />
-        <br />
-        <Title>버스 이용시</Title>
+        강동 KDW웨딩홀 3층 블랙스톤홀
         <br />
         <br />
-        410-1, 401 호텔수성 앞 하차
-        <br />
-        수성1-1, 수성3-1, 814 TBC방송국 앞 하차
+        <Title>지하철</Title>
         <br />
         <br />
-        <Title>지하철 이용시</Title>
-        <br />
-        <br />
-        3호선 수성못역 하차 (도보 10분)
+        5호선 강동역 하차 (도보 5분)
       </Content>
     </Wrapper>
   );
