3f8744e7ce3539b50299cc658e1f472ebbbb34ee
wonhee-wedding-invitation <woojw0829@gmail.com>
2025-12-13 16:19:52 +0900

    Fix: 식사 장소 주석

--- src/component/information/index.tsx
+++ src/component/information/index.tsx
@@ -13,8 +13,8 @@ export const Information1 = () => {
         <div className="label">식사 안내</div>
         <div className="content">
           식사시간: 14시 00분 ~ 16시 00분
-          <br />
-          장소: 지하 1층 연회장
+          {/* <br />
+          장소: 지하 1층 연회장 */}
         </div>
       </div>
     </>
